93193fcac776622611a6053a9201c7cdd85f3d01
Pratik Tikande <105018505+pratiktikande@users.noreply.github.com>
2025-06-24 00:06:52 +0530

    Final Answers

---
+++ Answers.java
@@ -0,0 +1,5 @@
+public class Answers extends QuizMain{
+    public static char[] getCorrectAnswers() {
+        return new char[] {'B', 'C', 'D', 'C', 'C' ,'C' , 'C' , 'B' ,'B' ,'C' , 'D'  }; 
+    }
+}
